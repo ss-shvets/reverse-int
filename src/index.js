98db24f460a9b49de49e2ee5f318f7ef1b54ec95
ss-shvets <oleksandr.shvets@protonmail.com>
2022-07-15 20:57:57 +0300

    feat: add reason for project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const isNegativeArgument = (n) => {
+        return Math.sign(n);
+    };
+    const reverseNumber = (n) => {
+        return n.split("").reverse().join("");
+    };
+    function reverseNum(n) {
+        return (
+            parseFloat(
+                n
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+            ) * -1 * Math.sign(n)
+        )
+    }
+    if (isNegativeArgument(n) === -1) {
+        return(reverseNum(n))
+    }
+
+
+
+    if (isNegativeArgument(n) === 1) {
+        return reverseNumber(n + '');
+    }
 }
